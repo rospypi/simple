3883db23e956657259cb9bc475bd493b5e796ac1
Tamaki Nishino <otamachan@gmail.com>
2019-08-17 21:52:15 +0900

    Change branch

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd index || exit
+BRANCH="$(uname -s)"
 git init
-git checkout -b gh-pages
+git checkout -b "$BRANCH"
 git add .
 git commit -m "Release $(date --rfc-3339=sec)"
 git remote add origin https://github.com/otamachan/rospy3.git
-git push -f origin gh-pages
-rm .git -rf
+git push -f origin "$BRANCH"
+rm -rf .git
