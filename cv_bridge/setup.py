90e19938d6be39eb0fed6c1dcc67f36e68a482b9
Tamaki Nishino <otamachan@gmail.com>
2019-10-19 14:36:37 +0900

    Add cv_bridge.boost

--- cv_bridge/setup.py
+++ cv_bridge/setup.py
@@ -1,1 +1,10 @@
-vision_opencv/cv_bridge/setup.py
+#!/usr/bin/env python
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup()
+
+d['packages'] = ['cv_bridge', 'cv_bridge.boost']
+d['package_dir'] = {'' : 'python'}
+
+setup(**d)
