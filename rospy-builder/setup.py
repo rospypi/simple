e36ab18bfd171ae117b016600a44a826edbc9f85
Tamaki Nishino <otamachan@gmail.com>
2020-01-13 09:17:07 +0900

    Add click and liint setup.py by black

--- rospy-builder/setup.py
+++ rospy-builder/setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
+
 setup(
-    name='rospy-builder',
-    version='0.2.0',
-    description='rospy package build tool',
-    author='Tamamki Nishino',
-    author_email='otamachan@gmail.com',
-    url='https://rospypi.github.io/simple',
-    packages=['rospy_builder'],
+    name="rospy-builder",
+    version="0.2.0",
+    description="rospy package build tool",
+    author="Tamamki Nishino",
+    author_email="otamachan@gmail.com",
+    url="https://rospypi.github.io/simple",
+    packages=["rospy_builder"],
     install_requires=[
-        'catkin_pkg',
-        'genmsg',
-        'genpy<2000',
-        'pyyaml',
-        'setuptools',
-        'gitpython',
-        ],
+        "catkin_pkg",
+        "click",
+        "genmsg",
+        "genpy<2000",
+        "pyyaml",
+        "setuptools",
+        "gitpython",
+    ],
     entry_points={
-        'console_scripts': [
-            'rospy-build = rospy_builder.build:cli',
-        ],
+        "console_scripts": ["rospy-build = rospy_builder.build:cli",],
     },
 )
