7e5090ed26ae66a4aa635386af07e0ab2b77ca10
Tamaki Nishino <otamachan@gmail.com>
2020-09-30 07:06:49 +0900

    PEP 508 compilant dependency

--- rospy-builder/setup.py
+++ rospy-builder/setup.py
@@ -1,29 +1,25 @@
 from setuptools import setup
-import sys
 
-install_requires = [
-    "catkin_pkg",
-    "click",
-    "genmsg",
-    "genpy<2000",
-    "pyyaml",
-    "setuptools",
-    "gitpython",
-    "wheel",
-]
-
-if sys.version_info < (3, 7):
-    install_requires.append("dataclasses>=0.7,<1")
 
 setup(
     name="rospy-builder",
-    version="0.5.1",
+    version="0.5.2",
     description="rospy package build tool",
     author="Tamamki Nishino",
     author_email="otamachan@gmail.com",
     url="https://rospypi.github.io/simple",
     packages=["rospy_builder"],
-    install_requires=install_requires,
+    install_requires=[
+        "catkin_pkg",
+        "click",
+        "genmsg",
+        "genpy<2000",
+        "pyyaml",
+        "setuptools",
+        "gitpython",
+        "wheel",
+        "dataclasses>=0.7,<1 ; python_version < '3.7'",
+    ],
     entry_points={
         "console_scripts": ["rospy-build = rospy_builder.build:cli",],
     },
